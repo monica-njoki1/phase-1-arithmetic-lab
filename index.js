ea01a71037dc81bfadf05585b00915342c15b7fa
monica <monica.njoki@student.moringaschool.com>
2025-06-10 14:42:04 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1= 31;
+const num2= 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() *100) +1;
+
+let num3 = 54;
+let num4 = 10;
+let mod = num3 % num4;
+
+let max = Math.max(2, 0, 13, 20, 4, 8, 13, 15);
+
+
+
